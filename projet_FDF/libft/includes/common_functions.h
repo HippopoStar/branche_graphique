39ada35e76b977528382a93e773640762d34698c
Leo CABANES <lcabanes@student.42.fr>
2018-07-13 16:49:45 +0200

    Ajout du debut de "ft_wildstring_to_string"

--- projet_FDF/libft/includes/common_functions.h
+++ projet_FDF/libft/includes/common_functions.h
@@ -6,12 +6,14 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 05:09:19 by lcabanes          #+#    #+#             */
-/*   Updated: 2018/07/12 02:31:02 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/07/13 16:48:00 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_FUNCTIONS_H
 # define COMMON_FUNCTIONS_H
+
+#include <wchar.h>
 
 void			ft_putnbr_base(int nbr, char *base);
 char			**ft_split_whitespaces(char *str);
@@ -21,5 +23,6 @@ int				ft_melt_colors(int red, int green, int blue);
 void			ft_break_color_down(int *red, int *green, int *blue, int color);
 char			*ft_itoa_base\
 							(int n, const char *base, size_t pres, size_t spac);
+unsigned int	ft_wildchar_to_unsigned_int(wint_t wildchar);
 
 #endif
